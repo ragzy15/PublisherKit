6b6227a1c4a70e4a5cec1cd0c096a2ef91f01414
Raghav Ahuja <raghavahuja@me.com>
2020-01-30 16:30:11 +0530

    Moved notification sink in another extension

--- Sources/PublisherKit/Publishers/Notification Center/Notification Center.swift
+++ Sources/PublisherKit/Publishers/Notification Center/Notification Center.swift
@@ -57,55 +57,59 @@ extension NotificationCenter {
             notificationSubscriber.request(.unlimited)
             subscriber.receive(subscription: notificationSubscriber)
         }
+    }
+}
+
+extension NotificationCenter.PKPublisher {
+    
+    // MARK: NOTIFICATION CENTER SINK
+    private final class InternalSink<Downstream: PKSubscriber>: PKSubscribers.Sinkable<Downstream, Output, Failure> where Downstream.Failure == Failure, Downstream.Input == Output {
         
-        final private class InternalSink<Downstream: PKSubscriber>: Sinkable<Downstream, Output, Failure> where Downstream.Failure == Failure, Downstream.Input == Output {
-            
-            private let center: NotificationCenter
-            private let name: Notification.Name
-            private let object: AnyObject?
-            
-            private var observer: NSObjectProtocol?
-            
-            init(downstream: Downstream, center: NotificationCenter, name: Notification.Name, object: AnyObject?) {
-                self.center = center
-                self.name = name
-                self.object = object
-                super.init(downstream: downstream)
+        private let center: NotificationCenter
+        private let name: Notification.Name
+        private let object: AnyObject?
+        
+        private var observer: NSObjectProtocol?
+        
+        init(downstream: Downstream, center: NotificationCenter, name: Notification.Name, object: AnyObject?) {
+            self.center = center
+            self.name = name
+            self.object = object
+            super.init(downstream: downstream)
+        }
+        
+        func observe() {
+            observer = center.addObserver(forName: name, object: object, queue: nil) { [weak self] (notification) in
+                self?.receive(input: notification)
             }
-            
-            func observe() {
-                observer = center.addObserver(forName: name, object: object, queue: nil) { [weak self] (notification) in
-                    self?.receive(input: notification)
-                }
+        }
+        
+        override func receive(_ input: Output) -> PKSubscribers.Demand {
+            guard !isCancelled else { return .none }
+            downstream?.receive(input: input)
+            return demand
+        }
+        
+        override func receive(completion: PKSubscribers.Completion<Failure>) {
+            guard !isCancelled else { return }
+            end()
+            downstream?.receive(completion: completion)
+        }
+        
+        override func end() {
+            if let observer = observer {
+                center.removeObserver(observer, name: name, object: object)
             }
-            
-            override func receive(_ input: Output) -> PKSubscribers.Demand {
-                guard !isCancelled else { return .none }
-                downstream?.receive(input: input)
-                return demand
+            observer = nil
+            super.end()
+        }
+        
+        override func cancel() {
+            if let observer = observer {
+                center.removeObserver(observer, name: name, object: object)
             }
-            
-            override func receive(completion: PKSubscribers.Completion<Failure>) {
-                guard !isCancelled else { return }
-                end()
-                downstream?.receive(completion: completion)
-            }
-            
-            override func end() {
-                if let observer = observer {
-                    center.removeObserver(observer, name: name, object: object)
-                }
-                observer = nil
-                super.end()
-            }
-            
-            override func cancel() {
-                if let observer = observer {
-                    center.removeObserver(observer, name: name, object: object)
-                }
-                observer = nil
-                super.cancel()
-            }
+            observer = nil
+            super.cancel()
         }
     }
 }
