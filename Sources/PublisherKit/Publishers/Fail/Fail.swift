071f898659067ea56916701a2ff373517fc0dfa1
Raghav Ahuja <ragzy15@gmail.com>
2020-03-14 14:04:33 +0530

    Added Fail Publisher

---
+++ Sources/PublisherKit/Publishers/Fail/Fail.swift
@@ -0,0 +1,54 @@
+//
+//  Fail.swift
+//  PublisherKit
+//
+//  Created by Raghav Ahuja on 13/03/20.
+//
+
+/// A publisher that immediately terminates with the specified error.
+public struct Fail<Output, Failure: Error>: Publisher {
+    
+    /// The failure to send when terminating the publisher.
+    public let error: Failure
+    
+    /// Creates a publisher that immediately terminates with the specified failure.
+    ///
+    /// - Parameter error: The failure to send when terminating the publisher.
+    public init(error: Failure) {
+        self.error = error
+    }
+    
+    /// Creates publisher with the given output type, that immediately terminates with the specified failure.
+    ///
+    /// Use this initializer to create a `Fail` publisher that can work with subscribers or publishers that expect a given output type.
+    /// - Parameters:
+    ///   - outputType: The output type exposed by this publisher.
+    ///   - failure: The failure to send when terminating the publisher.
+    public init(outputType: Output.Type, failure: Failure) {
+        self.error = failure
+    }
+    
+    public func receive<S: Subscriber>(subscriber: S) where Output == S.Input, Failure == S.Failure {
+        
+        let failSubscription = Inner(downstream: subscriber)
+        
+        subscriber.receive(subscription: failSubscription)
+        failSubscription.request(.max(1))
+        
+        failSubscription.receive(completion: .failure(error))
+    }
+}
+
+extension Fail: Equatable where Failure: Equatable {
+}
+
+extension Fail {
+    
+    // MARK: FAIL SINK
+    private final class Inner<Downstream: Subscriber>: Subscriptions.Internal<Downstream, Output, Failure> where Output == Downstream.Input, Failure == Downstream.Failure {
+        
+        override var description: String {
+            "Fail"
+        }
+    }
+}
