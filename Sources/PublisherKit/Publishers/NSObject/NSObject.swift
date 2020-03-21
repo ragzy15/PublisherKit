15972eb6cc6320729b87c260e6afce29768fb9a5
Raghav Ahuja <ragzy15@gmail.com>
2020-03-22 01:46:52 +0530

    Updated NSObject KeyValueObservingPKPublisher

--- Sources/PublisherKit/Publishers/NSObject/NSObject.swift
+++ Sources/PublisherKit/Publishers/NSObject/NSObject.swift
@@ -7,9 +7,6 @@
 
 import Foundation
 
-extension NSObject: PublisherCompatible {
-}
-
 extension NSObject {
     
     /// A publisher that emits events when the value of a KVO-compliant property changes.
@@ -35,12 +32,7 @@ extension NSObject {
         }
         
         public func receive<S: Subscriber>(subscriber: S) where Output == S.Input, Failure == S.Failure {
-            
-            let nsObjectSubscriber = Inner(downstream: subscriber, object: object, keyPath: keyPath, options: options)
-            
-            subscriber.receive(subscription: nsObjectSubscriber)
-            
-            nsObjectSubscriber.observe()
+            subscriber.receive(subscription: KVOSubscription(downstream: AnySubscriber(subscriber), object: object, keyPath: keyPath, options: options))
         }
         
         public static func == (lhs: NSObject.KeyValueObservingPKPublisher<Subject, Value>, rhs: NSObject.KeyValueObservingPKPublisher<Subject, Value>) -> Bool {
@@ -49,48 +41,94 @@ extension NSObject {
     }
 }
 
-extension NSObject.KeyValueObservingPKPublisher {
+extension NSObject: PublisherCompatible {
     
     // MARK: NSOBJECT SINK
-    private final class Inner<Downstream: Subscriber>: Subscriptions.Internal<Downstream, Output, Failure> where Output == Downstream.Input, Failure == Downstream.Failure {
+    final class KVOSubscription<Subject: NSObject, Output, Failure: Error>: Subscription, CustomStringConvertible, CustomReflectable {
         
         private var observer: NSKeyValueObservation?
         
         private var object: Subject?
         
-        private let keyPath: KeyPath<Subject, Value>
+        private let keyPath: KeyPath<Subject, Output>
         
         private let options: NSKeyValueObservingOptions
         
-        init(downstream: Downstream, object: Subject, keyPath: KeyPath<Subject, Value>, options: NSKeyValueObservingOptions) {
+        private let lock = Lock()
+        private let downstreamLock = RecursiveLock()
+        
+        private var downstream: AnySubscriber<Output, Failure>?
+        private var demand: Subscribers.Demand = .none
+        
+        init(downstream: AnySubscriber<Output, Failure>, object: Subject, keyPath: KeyPath<Subject, Output>, options: NSKeyValueObservingOptions) {
+            self.downstream = downstream
             self.object = object
             self.keyPath = keyPath
             self.options = options
-            super.init(downstream: downstream)
         }
         
-        func observe() {
-            observer = object?.observe(keyPath, options: options) { [weak self] (object, valueChange) in
+        func request(_ demand: Subscribers.Demand) {
+            precondition(demand > .none, "Demand must not be zero.")
+            
+            lock.lock()
+            guard observer == nil else { lock.unlock(); return }
+            self.demand += demand
+            lock.unlock()
+            
+            let observer = object?.observe(keyPath, options: options) { [weak self] (object, valueChange) in
+                guard let `self` = self else { return }
+                
+                self.lock.lock()
+                guard self.demand > .none else { self.lock.unlock(); return }
+                self.demand -= 1
+                self.lock.unlock()
+                
+                self.downstreamLock.lock()
+                var additionalDemand: Subscribers.Demand = .none
                 
                 if let oldValue = valueChange.oldValue {
-                    self?.receive(input: oldValue)
+                    additionalDemand += self.downstream?.receive(oldValue) ?? .none
                 }
                 
                 if let newValue = valueChange.newValue {
-                    self?.receive(input: newValue)
+                    additionalDemand += self.downstream?.receive(newValue) ?? .none
                 }
+                self.downstreamLock.unlock()
+                
+                self.lock.lock()
+                self.demand += additionalDemand
+                self.lock.unlock()
             }
+            
+            lock.lock()
+            self.observer = observer
+            lock.unlock()
         }
         
-        override func cancel() {
-            super.cancel()
-            observer?.invalidate()
-            observer = nil
+        func cancel() {
+            lock.lock()
+            guard let observer = observer else { lock.unlock(); return }
+            self.observer = nil
             object = nil
+            downstream = nil
+            lock.unlock()
+            observer.invalidate()
         }
         
-        override var description: String {
-            "NSObject KeyValueObserving"
+        var description: String {
+            "KVOSubscription"
+        }
+        
+        var customMirror: Mirror {
+            lock.lock()
+            defer { lock.unlock() }
+            
+            let children: [Mirror.Child] = [
+                ("observation", observer as Any),
+                ("demand", demand)
+            ]
+            
+            return Mirror(self, children: children)
         }
     }
 }
